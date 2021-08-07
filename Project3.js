76a306704ee00cfcafd2db850a4126051c005688
RhyanCodes <77405871+RhyanCodes@users.noreply.github.com>
2021-08-06 23:00:18 -0400

    Update Project3.js

--- Project3.js
+++ Project3.js
@@ -1,100 +1,113 @@
-document.querySelector('.check-box').addEventListener('click', function (){
-  const inputNumber = Number(document.querySelector('.input-box').value) ;
-  console.log(randomNumber);
-
-  if(questionText.textContent!='?')
-  {
-
-      document.body.style.background = "linear-gradient(to top right, #9198e5, #f78773, #ffaf8a)" ;
-      checkBox.style.backgroundColor = "skyblue";
-      checkBox.style.border = "2px solid yellow";
-
+let randomNumber = generateNumber();
+let score = document.querySelector('.score-text');
+let scoreCounter = document.querySelector('.scoreText').textContent;
+let highScore = document.querySelector('.highScore');
+const hintText = document.querySelector('.hintWord');
+let questionText = document.querySelector('.question-text');
+let guessingHistory = document.querySelector('.history');
+let checkBox = document.querySelector('.guess2');
+let inputBox = document.querySelector('.guess1');
 
 
-      scoreCounter = 10 ;
-      score.textContent = 10;
-      questionText.textContent = '?' ;
-      inputBox.value = '';
-      hintText.textContent = `Let's start Guessing...`;
+document.querySelector('.guess2').addEventListener('click', function (){
+    const inputNumber = Number(document.querySelector('.guess1').value) ;
+    console.log(randomNumber);
+
+    if(questionText.textContent!='?')
+    {
+
+        document.body.style.background = "linear-gradient(to top right, #9198e5, #f78773, #ffaf8a)" ;
+        checkBox.style.backgroundColor = "skyblue";
+        checkBox.style.border = "2px solid yellow";
 
 
 
-      randomNumber = generateNumber();
-  }
-
-  else if(inputNumber=='' || !Number(inputNumber) || inputNumber>100 || inputNumber<1){
-      hintText.textContent = `Invalid input` ;
-  }
-
-  else if(checkNum(inputNumber,randomNumber)){
+        scoreCounter = 10 ;
+        score.textContent = 10;
+        questionText.textContent = '?' ;
+        inputBox.value = '';
+        hintText.textContent = `Let's start Guessing...`;
 
 
-      hintText.textContent = `👍 Correct!`;
-      if(Number(highScore.textContent)<Number(scoreCounter)){highScore.textContent = scoreCounter ; }
-      questionText.textContent = inputNumber ;
-      scoreCounter = 10;
-      score.textContent = 10;
+
+        randomNumber = generateNumber();
+    }
+
+    else if(inputNumber=='' || !Number(inputNumber) || inputNumber>100 || inputNumber<1){
+        hintText.textContent = `Invalid input` ;
+    }
 
 
-      questionText.style.background = 'pink';
-      document.body.style.background = "linear-gradient(to top right,#4f705e,#62e39c)" ;
-      checkBox.style.background = "grey";
-      checkBox.style.border = "1px solid transparent" ;
-
-  }
+    else if(checkNum(inputNumber,randomNumber)){
 
 
-  else{
-      hintText.textContent = getHint(inputNumber,randomNumber) ;
-      scoreCounter--;
-
-      if(lose(scoreCounter)) {
-          hintText.textContent = 'You lost.......' ;
-          scoreCounter = 10 ;
-          score.textContent = 10;
-          randomNumber = generateNumber() ;
-      }
+        hintText.textContent = `👍 Correct!`;
+        if(Number(highScore.textContent)<Number(scoreCounter)){highScore.textContent = scoreCounter ; }
+        questionText.textContent = inputNumber ;
+        scoreCounter = 10;
+        score.textContent = 10;
 
 
-  }
-  score.textContent = scoreCounter ;
+        questionText.style.background = 'pink';
+        document.body.style.background = "linear-gradient(to top right,#4f705e,#62e39c)" ;
+        checkBox.style.background = "grey";
+        checkBox.style.border = "1px solid transparent" ;
+
+    }
+
+
+    else{
+        hintText.textContent = getHint(inputNumber,randomNumber) ;
+        scoreCounter--;
+
+        if(lose(scoreCounter)) {
+            hintText.textContent = 'You lost.......' ;
+            scoreCounter = 10 ;
+            score.textContent = 10;
+            randomNumber = generateNumber() ;
+        }
+
+
+    }
+    score.textContent = scoreCounter ;
 }
 )
 
 
-document.querySelector('.reset').addEventListener('click',function(){
 
-  document.body.style.background = "linear-gradient(to top right, #9198e5, #f78773, #ffaf8a)" ;
-  checkBox.style.backgroundColor = "skyblue";
-  checkBox.style.border = "2px solid yellow"
+document.querySelector('.play').addEventListener('click',function(){
 
 
-  scoreCounter = 10 ;
-  score.textContent = 10;
-  inputBox.value = '';
-  hintText.textContent = `Let's start Guessing...`;
+    document.body.style.background = "linear-gradient(to top right, #9198e5, #f78773, #ffaf8a)" ;
+    checkBox.style.backgroundColor = "skyblue";
+    checkBox.style.border = "2px solid yellow"
 
 
-  randomNumber = generateNumber();
+    scoreCounter = 10 ;
+    score.textContent = 10;
+    inputBox.value = '';
+    hintText.textContent = `Let's start Guessing...`;
 
+
+    randomNumber = generateNumber();
 })
 
 function generateNumber(){
-  return Math.trunc(Math.random()*100) + 1 ;
+    return Math.trunc(Math.random()*100) + 1 ;
 }
 
 function checkNum(inputNum,correctNum){
-  if(inputNum!=correctNum){return false;}
-  else{return true;}
+    if(inputNum!=correctNum){return false;}
+    else{return true;}
 }
 
 function getHint(inputNum, correctNum){
-  if(inputNum>correctNum){return `Too High !`;}
-  else{return `Too Low !`;}
+    if(inputNum>correctNum){return `Too High !`;}
+    else{return `Too Low !`;}
 }
 
 function lose(scoreCounter)
 {
-  if(scoreCounter!=0){return false;}
-  else{return true;}
+    if(scoreCounter!=0){return false;}
+    else{return true;}
 }
